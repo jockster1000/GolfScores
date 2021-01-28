8787d34ac7dd1ecc172f73e2d83731353121d582
Steve Richardson <jockster1000@msn.com>
2021-01-28 11:35:56 +0000

    Added unit test project for players service

---
+++ GolfScores.Services.Players.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace GolfScores.Services.Players.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
