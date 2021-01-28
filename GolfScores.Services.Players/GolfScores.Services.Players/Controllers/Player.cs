1394f5e89ac45ee27b41183824d0e827823682d6
Steve Richardson <jockster1000@msn.com>
2021-01-28 11:19:41 +0000

    added players service

---
+++ GolfScores.Services.Players/GolfScores.Services.Players/Controllers/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace GolfScores.Services.Players.Controllers
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public double Handicap { get; set; }
+    }
+}
---
+++ GolfScores.Services.Players/GolfScores.Services.Players/Controllers/PlayersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GolfScores.Services.Players.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayersController : ControllerBase
+    {
+        private readonly ILogger<PlayersController> _logger;
+
+        public PlayersController(ILogger<PlayersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Player> Get()
+        {
+            return new List<Player>
+            {
+                new Player { Id = 1, Firstname = "Steven", Lastname = "Richardson", Handicap = 4.1 },
+                new Player { Id = 2, Firstname = "Tiger", Lastname = "Woods", Handicap = 0 },
+                new Player { Id = 1, Firstname = "Rory", Lastname = "Mcilroy", Handicap = 0 }
+            };
+        }
+    }
+}
